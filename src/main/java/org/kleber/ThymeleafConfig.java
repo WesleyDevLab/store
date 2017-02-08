4910186b9c46467f695213c90248c6d37553d078
Kleber Mota <klebermo@gmail.com>
2017-02-07 21:55:23 -0300

    ThymeleafConfig.java

--- src/main/java/org/kleber/ThymeleafConfig.java
+++ src/main/java/org/kleber/ThymeleafConfig.java
@@ -4,6 +4,7 @@ import org.kleber.thymeleaf.dialect.Field;
 import org.kleber.thymeleaf.dialect.Form;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 
 @Configuration
@@ -12,6 +13,7 @@ public class ThymeleafConfig {
 	@Bean
 	public SpringTemplateEngine templateEngine() {
 		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.addDialect(new SpringSecurityDialect());
 		engine.addDialect(new Form());
 		engine.addDialect(new Field());
 		return engine;
