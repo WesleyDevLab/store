e2eb2861202fb62fb2b55559dec68a4109d0991a
Kleber Mota <klebermo@gmail.com>
2017-02-09 08:27:49 -0300

    Fieldset.java

--- src/main/java/org/kleber/thymeleaf/processor/field/Fieldset.java
+++ src/main/java/org/kleber/thymeleaf/processor/field/Fieldset.java
@@ -1,26 +1,18 @@
 package org.kleber.thymeleaf.processor.field;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
 import org.thymeleaf.Arguments;
 import org.thymeleaf.dom.Element;
-import org.thymeleaf.processor.ProcessorResult;
-import org.thymeleaf.processor.element.AbstractElementProcessor;
+import org.thymeleaf.processor.element.AbstractConditionalVisibilityElementProcessor;
 
-public class Fieldset extends AbstractElementProcessor {
-	
+public class Fieldset extends AbstractConditionalVisibilityElementProcessor {
+
 	private String type;
-
+	
 	public Fieldset() {
-		super("fieldset");
-	}
-
-	@Override
-	public ProcessorResult processElement(Arguments arguments, Element element) {
-		return ProcessorResult.OK;
-	}
-
-	@Override
-	public int getPrecedence() {
-		return 0;
+		super("field-set");
 	}
 
 	public String getType() {
@@ -29,6 +21,27 @@ public class Fieldset extends AbstractElementProcessor {
 
 	public void setType(String type) {
 		this.type = type;
+	}
+
+	@Override
+	public boolean isVisible(Arguments arguments, Element element) {
+		type = element.getAttributeValue("type");
+		Field field = (Field) arguments.getLocalVariable("field");
+		for(Annotation annotation : field.getAnnotations()) {
+			if(annotation.annotationType().getSimpleName().equals(type))
+				return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean removeHostElementIfVisible(Arguments arguments, Element element) {
+		return true;
+	}
+
+	@Override
+	public int getPrecedence() {
+		return 0;
 	}
 
 }
