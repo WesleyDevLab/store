0dc64c065da08a2478a29184211c804c0d376855
Kleber Mota <klebermo@gmail.com>
2017-02-09 08:28:11 -0300

    Geral.java

--- src/main/java/org/kleber/settings/geral/Geral.java
+++ src/main/java/org/kleber/settings/geral/Geral.java
@@ -1,16 +1,24 @@
 package org.kleber.settings.geral;
 
-import org.kleber.settings.Property;
+import org.kleber.annotation.class_type.Property;
+import org.kleber.annotation.input_type.Email;
+import org.kleber.annotation.input_type.Text;
+import org.kleber.annotations.field.Input;
 
-public class Geral extends Property {
+@Property
+public class Geral extends org.kleber.settings.Property {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1336603477955595950L;
 	
+	@Input
+	@Text
 	private String autor;
 	
+	@Input
+	@Email
 	private String email;
 	
 	public String getAutor() {
