54d930cf996f9c1953e5e5dc86b5a184d272f848
Kleber Mota <klebermo@gmail.com>
2017-02-08 21:37:14 -0300

    Form.java

--- src/main/java/org/kleber/thymeleaf/processor/form/Form.java
+++ src/main/java/org/kleber/thymeleaf/processor/form/Form.java
@@ -1,19 +1,45 @@
 package org.kleber.thymeleaf.processor.form;
 
-import org.thymeleaf.Arguments;
-import org.thymeleaf.dom.Element;
-import org.thymeleaf.processor.ProcessorResult;
-import org.thymeleaf.processor.element.AbstractElementProcessor;
+import java.util.Map;
 
-public class Form extends AbstractElementProcessor {
+import org.thymeleaf.Arguments;
+import org.thymeleaf.dom.Attribute;
+import org.thymeleaf.dom.Element;
+import org.thymeleaf.processor.element.AbstractIterationElementProcessor;
+
+public class Form extends AbstractIterationElementProcessor {
 
 	public Form() {
 		super("form");
 	}
 
 	@Override
-	protected ProcessorResult processElement(Arguments arguments, Element element) {
-		return ProcessorResult.OK;
+	public String getIteratedElementName(Arguments arguments, Element element) {
+		return "form";
+	}
+
+	@Override
+	public IterationSpec getIterationSpec(Arguments arguments, Element element) {
+		Object command = arguments.getContext().getVariables().get("command");
+		return new AbstractIterationElementProcessor.IterationSpec("field", "status", command.getClass().getDeclaredFields());
+	}
+
+	@Override
+	public void processClonedHostIterationElement(Arguments arguments, Element element) {
+		Element form = new Element("form");
+		for( Map.Entry<String, Attribute> entry : element.getAttributeMap().entrySet() )
+			form.setAttribute(entry.getKey(), entry.getValue().getValue());
+		form.setAttribute("method", "post");
+		form.setAttribute("action", null);
+		form.setProcessable(false);
+		element.getParent().insertBefore(element, form);
+		element.getParent().removeChild(element);
+		
+	}
+
+	@Override
+	public boolean removeHostIterationElement(Arguments arguments, Element element) {
+		return false;
 	}
 
 	@Override
