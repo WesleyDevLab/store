cb27adafe90d28843c8648e6870b8e61f7e145cc
Kleber Mota <klebermo@gmail.com>
2017-02-09 11:51:21 -0300

    SecurityConfig.java

--- src/main/java/org/kleber/SecurityConfig.java
+++ src/main/java/org/kleber/SecurityConfig.java
@@ -2,9 +2,9 @@ package org.kleber;
 
 import java.io.Serializable;
 
+import org.kleber.model.usuario.Usuario;
 import org.kleber.model.usuario.UsuarioDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.PermissionEvaluator;
@@ -16,10 +16,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
@@ -27,11 +29,6 @@ import org.springframework.security.web.access.expression.DefaultWebSecurityExpr
 @EnableWebSecurity
 @EnableGlobalMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
-	@Autowired
-	private PermissionEvaluator permissionEvaluator;
 	
 	public void configure(HttpSecurity http) throws Exception {
 		http
@@ -56,30 +53,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	public void configure(WebSecurity web) {
 		DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
-		handler.setPermissionEvaluator(permissionEvaluator);
+		handler.setPermissionEvaluator(permissionEvaluator());
 		web.expressionHandler(handler);
 	}
 	
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.userDetailsService(userDetailsService)
-			.passwordEncoder(new BCryptPasswordEncoder());
+			.userDetailsService(userDetailsService())
+			.passwordEncoder(passwordEncoder());
 	}
 	
-	@Bean
 	public UserDetailsService userDetailsService() {
 		return new UserDetailsService() {
 			@Autowired
 			private UsuarioDao usuario;
 
 			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				return (UserDetails) usuario.findBy("login", username);
+				Usuario account = usuario.findBy("login", username);
+				return new User(account.getNome() + " " + account.getSobrenome(), account.getPassword(), account.getAuthorities());
 			}
 			
 		};
 	}
 	
-	@Bean
 	public PermissionEvaluator permissionEvaluator() {
 		return new PermissionEvaluator() {
 
@@ -104,5 +100,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			}
 
 		};
+	}
+	
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
