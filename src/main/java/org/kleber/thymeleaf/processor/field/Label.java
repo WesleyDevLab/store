bba46211e026d1c7064715f5581fd443d8da65f3
Kleber Mota <klebermo@gmail.com>
2017-02-09 08:28:49 -0300

    Label.java

--- src/main/java/org/kleber/thymeleaf/processor/field/Label.java
+++ src/main/java/org/kleber/thymeleaf/processor/field/Label.java
@@ -1,7 +1,14 @@
 package org.kleber.thymeleaf.processor.field;
 
+import java.lang.reflect.Field;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
 import org.thymeleaf.Arguments;
+import org.thymeleaf.dom.Attribute;
 import org.thymeleaf.dom.Element;
+import org.thymeleaf.dom.Text;
 import org.thymeleaf.processor.ProcessorResult;
 import org.thymeleaf.processor.element.AbstractElementProcessor;
 
@@ -13,6 +20,24 @@ public class Label extends AbstractElementProcessor {
 
 	@Override
 	public ProcessorResult processElement(Arguments arguments, Element element) {
+		Field field = (Field) arguments.getLocalVariable("field");
+		
+		if(field != null) {
+			Element label = new Element("label");
+			for( Map.Entry<String, Attribute> entry : element.getAttributeMap().entrySet() )
+				label.setAttribute(entry.getKey(), entry.getValue().getValue());
+			
+			Locale currentLocale = Locale.getDefault();
+			ResourceBundle messages = ResourceBundle.getBundle("messages", currentLocale);
+			String msg = messages.getString(field.getName());
+			Text txt = new Text(msg);
+			label.addChild(txt);
+			
+			label.setProcessable(false);
+			element.getParent().insertBefore(element, label);
+			element.getParent().removeChild(element);
+		}
+		
 		return ProcessorResult.OK;
 	}
 
