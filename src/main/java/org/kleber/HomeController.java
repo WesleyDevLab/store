bc7608106a26c7ab7ed0e9e2b7ecf905b73fcb32
Kleber Mota <klebermo@gmail.com>
2017-02-08 21:37:55 -0300

    HomeController.java

--- src/main/java/org/kleber/HomeController.java
+++ src/main/java/org/kleber/HomeController.java
@@ -1,12 +1,30 @@
 package org.kleber;
 
+import org.kleber.model.categoria.CategoriaService;
+import org.kleber.model.pagina.PaginaService;
+import org.kleber.model.produto.ProdutoService;
+import org.kleber.model.usuario.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private CategoriaService categoria;
+	
+	@Autowired
+	private ProdutoService produto;
+	
+	@Autowired
+	private PaginaService pagina;
+	
+	@Autowired
+	private UsuarioService usuario;
+	
 	@RequestMapping("/")
 	public String index() {
 		return "index";
@@ -29,17 +47,20 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/c/{id}")
-	public String categoria(@PathVariable("id") String id) {
+	public String categoria(Model model, @PathVariable("id") String id) {
+		model.addAttribute("categoria", categoria.getObject(id));
 		return "categoria";
 	}
 	
 	@RequestMapping("/p/{id}")
-	public String produto(@PathVariable("id") String id) {
+	public String produto(Model model, @PathVariable("id") String id) {
+		model.addAttribute("produto", produto.getObject(id));
 		return "produto";
 	}
 	
 	@RequestMapping("/page/{titulo}")
-	public String pagina(@PathVariable("titulo") String titulo) {
+	public String pagina(Model model, @PathVariable("titulo") String titulo) {
+		model.addAttribute("pagina", null);
 		return "pagina";
 	}
 	
@@ -56,5 +77,11 @@ public class HomeController {
 	@RequestMapping("/order")
 	public String order() {
 		return "order";
+	}
+	
+	@RequestMapping("/settings")
+	public String settings(Model model) {
+		model.addAttribute("settings", null);
+		return "admin";
 	}
 }
