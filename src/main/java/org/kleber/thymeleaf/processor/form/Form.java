4ba9a24a27edf5dceef021b6f434a4bb0e0afe12
Kleber Mota <klebermo@gmail.com>
2017-02-10 09:14:30 -0300

    Form.java

--- src/main/java/org/kleber/thymeleaf/processor/form/Form.java
+++ src/main/java/org/kleber/thymeleaf/processor/form/Form.java
@@ -1,12 +1,10 @@
 package org.kleber.thymeleaf.processor.form;
 
-import java.lang.reflect.Field;
 import java.util.Map;
 
 import org.thymeleaf.Arguments;
 import org.thymeleaf.dom.Attribute;
 import org.thymeleaf.dom.Element;
-import org.thymeleaf.dom.Node;
 import org.thymeleaf.processor.ProcessorResult;
 import org.thymeleaf.processor.element.AbstractElementProcessor;
 
@@ -28,12 +26,6 @@ public class Form extends AbstractElementProcessor {
 				form.setAttribute(entry.getKey(), entry.getValue().getValue());
 			form.setAttribute("method", "post");
 			form.setAttribute("action", null);
-			for(Node child : element.getChildren()) {
-				for(Field field : command.getClass().getDeclaredFields()) {
-					child.setNodeProperty("field", field);
-					form.addChild(child.cloneNode(form, true));
-				}
-			}
 			form.setProcessable(false);
 			element.getParent().insertBefore(element, form);
 			element.getParent().removeChild(element);
