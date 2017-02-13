6f2b9d66c6308531830f435cbda0928c27012338
Kleber Mota <klebermo@gmail.com>
2017-02-13 09:05:00 -0300

    dialect Util and its processors

--- src/main/java/org/kleber/thymeleaf/dialect/Util.java
+++ src/main/java/org/kleber/thymeleaf/dialect/Util.java
@@ -8,18 +8,14 @@ import org.thymeleaf.processor.IProcessor;
 
 public class Util extends AbstractDialect {
 
-	protected Util() {
-		super("util");
-	}
-
 	public String getPrefix() {
 		return null;
 	}
 	
 	public Set<IProcessor> getProcessors() {
 		Set<IProcessor> processors = new HashSet<IProcessor>();
-		processors.add(new org.kleber.thymeleaf.processor.util.If());
-		processors.add(new org.kleber.thymeleaf.processor.util.List());
+		//processors.add(new org.kleber.thymeleaf.processor.util.If());
+		///processors.add(new org.kleber.thymeleaf.processor.util.List());
 		return processors;
 	}
 
--- src/main/java/org/kleber/thymeleaf/processor/util/If.java
+++ src/main/java/org/kleber/thymeleaf/processor/util/If.java
@@ -1,20 +1,5 @@
 package org.kleber.thymeleaf.processor.util;
 
-import org.thymeleaf.context.ITemplateContext;
-import org.thymeleaf.engine.AttributeName;
-import org.thymeleaf.model.IProcessableElementTag;
-import org.thymeleaf.standard.processor.AbstractStandardConditionalVisibilityTagProcessor;
-import org.thymeleaf.templatemode.TemplateMode;
-
-public class If extends AbstractStandardConditionalVisibilityTagProcessor {
-
-	public If() {
-		super(TemplateMode.HTML, null, "if", 0);
-	}
-
-	@Override
-	public boolean isVisible(ITemplateContext arg0, IProcessableElementTag arg1, AttributeName arg2, String arg3) {
-		return false;
-	}
+public class If {
 
 }
